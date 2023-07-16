7be8eaf0f4ed5103dfe30727c0d112e9d83dc28f
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-07-16 15:42:18 +0530

    Java program to print numbers from 1 to N
    This is a Java program that prompts the user to enter a number 'N' and prints the numbers from 1 to N. The program handles input validation to ensure a valid number is provided. It utilizes the BufferedReader class for input reading and ensures immediate display of output using System.out.flush().
    
    The code includes necessary import statements, error handling for null input, and proper closing of the BufferedReader to avoid resource leakage.

--- printno.java
+++ printno.java
@@ -3,13 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class printno {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        for (int i = 1; i <= n; ++i) {
-    System.out.println("No." + i);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        
+        if (input != null) {
+            int n = Integer.parseInt(input);
+            
+            for (int i = 1; i <= n; ++i) {
+                System.out.println("No." + i);
+                System.out.flush(); // Flush the output to ensure immediate display
+            }
+        } else {
+            System.out.println("Invalid input. Please provide a valid number.");
+        }
+        
+        br.close();
+    }
 }
-br.close();
-
-            }
-            }
