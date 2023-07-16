8120cc43a2bb171eaa577ebf2c1abe6f2d9ee05f
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-07-16 15:28:31 +0530

    update Hello-World.java

--- Hello-World.java
+++ Hello-World.java
@@ -1,7 +1,7 @@
 public class main {
   public static void main(String[] args) {
     System.out.println("Hello World");
-    // System.out.println("Hello World!");
+    System.out.println("Hello World!");
     System.out.println("I am learning Java.");
     System.out.println("It is awesome!");
   }
